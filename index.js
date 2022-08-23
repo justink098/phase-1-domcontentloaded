c64a8d949e725e316a66ceb5dfd4764cd23a3794
Justin <justin.kiarie@student.moringaschool.com>
2022-08-23 19:15:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+   let changecText = document.querySelector('p');
+   changecText.textContent = "This is really cool!"
+  });
+  
